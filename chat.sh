4a27481e2be8f3ecc29850c17feb2c9e81b43416
Sevada797 <64130935+Sevada797@users.noreply.github.com>
2022-06-23 23:57:15 +0400

    Create chat.sh

---
+++ chat.sh
@@ -0,0 +1,28 @@
+function loadchat() {
+clear
+echo "Welcome to secret chatroom!"
+echo "------Chat by Sevada------"
+echo "Chat was created using: bash, php and curl"
+echo -e "To reload the chat just press Enter to close the chat type 'exit!'\n"
+messages=$(curl -s https://somechat797.000webhostapp.com/chat.txt)
+echo -e "$messages"
+read -p "Type your message here: " msg
+if [ -z "$msg" ]
+then
+    loadchat
+elif [[ $msg == "exit!" ]]
+then
+    close
+    return 1
+elif [[ $msg != "" ]]
+then
+    curl "https://somechat797.000webhostapp.com/index.php?msg=$USER:%20$msg"
+    loadchat
+fi 
+}
+
+function close() {
+echo -e "\nYou quit the chat"
+}
+
+loadchat
