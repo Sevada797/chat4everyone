e1beac2af2d76c0e3daf3cef1d8da00901f7c512
Sevada797 <64130935+Sevada797@users.noreply.github.com>
2022-06-24 20:39:27 +0400

    Update chat.sh

--- chat.sh
+++ chat.sh
@@ -16,7 +16,7 @@ then
     return 1
 elif [[ $msg != "" ]]
 then
-    curl --get --data-urlencode "msg=$(whoami):%20$msg" "https://somechat797.000webhostapp.com/index.php"
+    curl -s --get --data-urlencode "msg=$(whoami):%20$msg" "https://somechat797.000webhostapp.com/index.php" > /dev/null
     loadchat
 fi 
 }
