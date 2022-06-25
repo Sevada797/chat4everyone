d15b328e83e7483b601a78caf88894416f3a44be
Sevada797 <64130935+Sevada797@users.noreply.github.com>
2022-06-25 15:40:52 +0400

    Update chat.sh

--- chat.sh
+++ chat.sh
@@ -1,9 +1,12 @@
 function loadchat() {
 clear
 echo "Welcome to my chat server!"
-echo "------Chat by Sevada------"
-echo "Chat was created using: bash, php and curl"
-echo -e "To reload the chat press enter to close the chat type 'exit!'\n"
+echo -e "+-----------------------------------+"
+echo "|  Contributor: Sevada | @Sevada797 |"
+echo "|  Created with bash, php and curl  |"
+echo "|  to reload the chat press <enter> |"
+echo "|  to exit the chat type 'exit!'    |"
+echo -e "+-----------------------------------+\n"
 messages=$(curl -s https://somechat797.000webhostapp.com/chat.txt)
 echo -e "$messages"
 read -p "Type your message here: " msg
@@ -18,11 +21,10 @@ elif [[ $msg != "" ]]
 then
     curl -s --get --data-urlencode "msg=$(whoami): $msg" "https://somechat797.000webhostapp.com/index.php" > /dev/null
     loadchat
-fi 
+fi
 }
 
 function close() {
 echo -e "\nYou quit the chat"
 }
-
 loadchat
